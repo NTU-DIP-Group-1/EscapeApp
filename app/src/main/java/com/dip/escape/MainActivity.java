90881b56aa9d024e51335c740618d9a266720642
IrynaShv <iryna.shvydchenko@gmail.com>
2018-02-12 21:08:01 +0800

    Code cleanup

--- app/src/main/java/com/dip/escape/MainActivity.java
+++ app/src/main/java/com/dip/escape/MainActivity.java
@@ -1,28 +1,19 @@
 package com.dip.escape;
 
-
 import android.app.Activity;
-import android.content.SharedPreferences;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
-import android.hardware.Camera;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
-import android.view.MenuItem;
-import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -37,20 +28,17 @@ import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
-import java.io.IOException;
-
 public class MainActivity  extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
-    private ImageView mImageView;
-    private SurfaceView mSurfaceView;
-    private Bitmap mImageBitmap;
     private Button mTakePhotoButton;
     private TextView mHexTextView;
     private View mColorSeen;
 
+    private Mat mRgba;
+    private Mat mRgbaF;
+    private Mat mRgbaT;
+
     private CameraBridgeViewBase mOpenCvCameraView;
-
     private boolean mTakePicture;
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -64,9 +52,7 @@ public class MainActivity  extends Activity implements CameraBridgeViewBase.CvCa
         mColorSeen = findViewById(R.id.color_seen);
 
         mOpenCvCameraView = (JavaCameraView) findViewById(R.id.camera_surface_view);
-
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
-
         mOpenCvCameraView.setCvCameraViewListener(this);
 
         mTakePhotoButton.setOnClickListener(new View.OnClickListener() {
@@ -106,25 +92,15 @@ public class MainActivity  extends Activity implements CameraBridgeViewBase.CvCa
                     mColorSeen.setBackgroundColor(color);
                 }
             });
-
-
         }
-
     }
 
     public Bitmap rotateImage(Bitmap src, float degree) {
-        // create new matrix object
         Matrix matrix = new Matrix();
-        // setup rotation degree
         matrix.postRotate(degree);
-        // return new bitmap rotated using matrix
         return Bitmap.createBitmap(src, 0, 0, src.getWidth(), src.getHeight(),
                 matrix, true);
     }
-
-    Mat mRgba;
-    Mat mRgbaF;
-    Mat mRgbaT;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -144,16 +120,14 @@ public class MainActivity  extends Activity implements CameraBridgeViewBase.CvCa
 
 
     @Override
-    public void onPause()
-    {
+    public void onPause() {
         super.onPause();
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
     }
 
     @Override
-    public void onResume()
-    {
+    public void onResume() {
         super.onResume();
         if (!OpenCVLoader.initDebug()) {
             OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_2, this, mLoaderCallback);
@@ -169,7 +143,6 @@ public class MainActivity  extends Activity implements CameraBridgeViewBase.CvCa
     }
 
     public void onCameraViewStarted(int width, int height) {
-
         mRgba = new Mat(height, width, CvType.CV_8UC4);
         mRgbaF = new Mat(height, width, CvType.CV_8UC4);
         mRgbaT = new Mat(width, width, CvType.CV_8UC4);
@@ -180,10 +153,7 @@ public class MainActivity  extends Activity implements CameraBridgeViewBase.CvCa
     }
 
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-
-        // TODO Auto-generated method stub
         mRgba = inputFrame.rgba();
-        // Rotate mRgba 90 degrees
         Core.transpose(mRgba, mRgbaT);
         Imgproc.resize(mRgbaT, mRgbaF, mRgbaF.size(), 0,0, 0);
         Core.flip(mRgbaF, mRgba, 1 );
@@ -197,15 +167,14 @@ public class MainActivity  extends Activity implements CameraBridgeViewBase.CvCa
                 new Scalar(255, 0, 0, 0), 2);
 
         if (mTakePicture) {
-
             Bitmap imageBitmap = Bitmap.createBitmap(mRgba.width(), mRgba.height(), Bitmap.Config.ARGB_8888);
             Utils.matToBitmap(mRgba, imageBitmap);
 
             captureColor(imageBitmap);
             mTakePicture = false;
         }
-
-        return mRgba; // This function must return
+        
+        return mRgba;
     }
 
 }
